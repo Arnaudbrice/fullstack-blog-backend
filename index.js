e23275dbf3eefaeca4a4e818d593c8828caa18cd
arnaudbrice <mephisteur@gmail.com>
2025-08-20 15:12:33 +0200

    add backend code

--- index.js
+++ index.js
@@ -16,10 +16,130 @@ app.use(
 
 // Serve static files from the public folder
 app.use(express.static(path.join(import.meta.dirname, "public")));
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+const sequelize = new Sequelize(process.env.PG_URI);
+//id, author, content, cover, date
+const Blog = sequelize.define("Blog", {
+  author: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  cover: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+    allowNull: false
+  }
 });
+
+await Blog.sync();
+
+app.get("/", async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.json({ message: "Database connected successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Database connection failed" });
+  }
+});
+
+// GET /blogs
+app.get("/blogs", async (req, res) => {
+  try {
+    const blogs = await Blog.findAll();
+    res.json(blogs);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// GET /blogs/:id
+app.get("/blogs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await Blog.findByPk(id);
+    if (!blog) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    res.json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// POST /blogs
+app.post("/blogs", async (req, res) => {
+  try {
+    const { author, content, cover, date } = req.body;
+
+    if (!author || !content || !cover || !date) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
+    const blog = await Blog.create({ author, content, cover, date });
+
+    res.status(201).json(blog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// PUT /blogs/:id
+
+app.put("/blogs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const { author, content, cover, date } = req.body;
+    console.log("req.body", req.body);
+
+    if (!author || !content || !cover || !date) {
+      res.status(400).json({ message: "Missing required fields" });
+      return;
+    }
+
+    const [rowCount, updatedBlogs] = await Blog.update(
+      { author, content, cover, date },
+      { where: { id: id }, returning: true }
+    );
+
+    console.log("updatedBlogs", updatedBlogs);
+    if (!rowCount) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    const updatedBlog = updatedBlogs[0];
+    res.status(200).json(updatedBlog);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// DELETE /blogs/:id
+app.delete("/blogs/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const rowCount = await Blog.destroy({ where: { id: id } });
+    if (!rowCount) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    res.status(204).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is listening on port port!`);
+  console.log(`Server is listening on port ${port}!`);
 });
